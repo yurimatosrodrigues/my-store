1255d8e007964703f271f3274c0a2102d03f2fd4
Yuri Matos Rodrigues <yurimatos.tuti@gmail.com>
2023-01-19 17:57:10 -0300

    routerLink to productDetails

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,24 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '', component: ProductListComponent},
+      {path: 'products/:productId', component: ProductDetailsComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
